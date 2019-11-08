d7e7dee6c2ea20111a8958a64be08509e57d59d2
MarcosGTS <55096707+MarcosGTS@users.noreply.github.com>
2019-11-08 11:58:11 -0300

    Add files via upload
    jogo_v:0.0.2

---
+++ Jogo_faculdade/Jogo_faculdade/geraçao.lua
@@ -0,0 +1,185 @@
+
+function g_monster(dt, limite, vetor_monster,t, window_w, window_h)
+    if t < 60 then
+        dificuldade = 125
+    elseif t > 60 and t < 90 then
+        dificuldade = 75
+    elseif t > 90 then
+        dificuldade = 25
+    end
+
+    if #vetor_monster < limite then
+        if love.math.random(0,dificuldade) < 1 then
+            local monster = {}
+            monster.tipo = math.floor(love.math.random(1,2.001))
+            if monster.tipo < 2 then
+                monster.h = 40
+                monster.w = 40
+                monster.sp = 70
+                monster.hp = 100
+                monster.atq = 15
+                monster.delay = 1.5
+                monster.bullets = {}
+            elseif monster.tipo >= 2   then
+                monster.h = 40
+                monster.w = 40
+                monster.sp = 90
+                monster.hp = 50
+                monster.atq = 35
+                monster.delay = 1.5
+                monster.bullets = {}
+            end
+            repeat
+                monster.x = love.math.random(-40, window_w + 40)
+            until monster.x < 0 or  monster.x > window_w
+            repeat
+                monster.y = love.math.random(-40, window_h + 40)
+            until monster.y < 0 or monster.y > window_h
+            monster.dx = math.abs(monster.x - monster.w)/2
+            monster.dy = math.abs(monster.y - monster.h)/2
+            table.insert(vetor_monster, monster)    
+        end
+    end
+end
+
+function monster_death(vetor_monster, vetor_coins)
+    for i=#vetor_monster,1, -1 do 
+        local monster = vetor_monster[i]
+        if monster.hp <= 0 then
+            table.remove(vetor_monster,i)
+            local droop = love.math.random(1,10)
+            if droop >= 2 and droop <= 4 then
+                moedas = 1
+            elseif droop >= 1 and droop <= 1.5 then
+                moedas = 3
+            else 
+                moedas = 0 
+            end
+            while moedas > 0 do
+                local coin = {}
+                coin.w = 10
+                coin.h = 10
+                coin.sp = 150
+                coin.x = love.math.random(monster.x, monster.x + 20) 
+                coin.y = love.math.random(monster.y, monster.y + 20)
+                
+                table.insert(vetor_coins,coin)
+                
+                moedas = moedas - 1
+            end
+        end
+    end
+end
+
+function draw_coins(vetor_coins)
+    for i=#vetor_coins, 1, -1 do 
+        local coin = coins[i]
+        love.graphics.rectangle("fill",coin.x, coin.y, coin.w, coin.h)
+    end
+end
+
+function draw_monster(vetor_monster)
+    for i=1 , #vetor_monster, 1 do
+        local monster = vetor_monster[i]
+        if monster.tipo < 2 then
+            love.graphics.setColor(0.2,0.8,0.2)
+        else
+            love.graphics.setColor(0,0, 205)
+        end
+        love.graphics.rectangle("fill", monster.x, monster.y,monster.w, monster.h)
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.rectangle("fill", monster.x - 7, monster.y - 15, 35 * (monster.hp/100), 5)
+    end    
+end
+
+
+function g_tiros(vetor_tiros, px, py, mx, my)    
+    local tiro = {}
+    tiro.x = px
+    tiro.y = py
+    tiro.px = px
+    tiro.py = py
+    tiro.w = 10
+    tiro.h = 10
+    tiro.sp = 450
+    tiro.mx = mx
+    tiro.my = my
+    tiro.dano = 25
+    table.insert(vetor_tiros,tiro)
+end
+
+
+
+function direcao_tiros(dt,vetor_tiros)
+    for i=#vetor_tiros, 1, -1 do 
+        local tiro = vetor_tiros[i]
+      
+        x = tiro.mx - tiro.px
+        y = tiro.my - tiro.py
+        
+        angulo = math.atan2(y,x)
+        propx = math.cos(angulo)
+        propy = math.sin(angulo)
+
+        tiro.x = tiro.x + tiro.sp *propx * dt
+        tiro.y = tiro.y + tiro.sp *propy * dt 
+       
+    end
+end
+
+function bullet_draw(vetor_tiros)
+    for i=#vetor_tiros,1,-1 do
+        local tiro = vetor_tiros[i] 
+        love.graphics.rectangle("fill", tiro.x, tiro.y, tiro.w, tiro.h)
+    end
+end
+
+function bullet_death (vetor_tiros, window_w, window_h )
+    for i= #vetor_tiros, 1, -1 do
+        local tiro = vetor_tiros[i]
+        if tiro.x > window_w or tiro.x < 0 then
+            table.remove(vetor_tiros,i)
+        elseif tiro.y < 0 or tiro.y > window_h then
+            table.remove(vetor_tiros,i)
+        end
+    end
+end
+
+function gmonster_bullets(vetor_mbullets, mx, my, px, py)
+    local mbullet = {}
+    mbullet.w = 35
+    mbullet.h = 35
+    mbullet.x = mx  
+    mbullet.y = my
+    mbullet.mx = mx
+    mbullet.my = my
+    mbullet.dano = 10  
+    mbullet.sp = 320
+    mbullet.xr = px
+    mbullet.yr = py
+    table.insert(vetor_mbullets, mbullet)
+
+end
+
+function monster_bulletd(dt, monster_bullets)
+    for i=#monster_bullets, 1, -1 do 
+        local mbullet = monster_bullets[i] 
+        xm = mbullet.xr - mbullet.mx
+        ym = mbullet.yr - mbullet.my
+
+        angm = math.atan2(ym,xm)
+        propxm = math.cos(angm)
+        propym = math.sin(angm)
+        
+        mbullet.x = mbullet.x + propxm * dt * mbullet.sp
+        mbullet.y = mbullet.y + propym * dt * mbullet.sp
+    end
+end
+
+function mbullet_draw(mbvetor)
+    for i=#mbvetor,1, -1 do
+        local mbullet = mbvetor[i]
+        love.graphics.rectangle("fill", mbullet.x, mbullet.y, mbullet.w, mbullet.h)
+    end
+end
+
---
+++ Jogo_faculdade/Jogo_faculdade/main.lua
@@ -0,0 +1,266 @@
+require("colision") -- para utilizar funçoes feitas em outro arquivo
+require("movimentaçao")
+require("geraçao")
+require("vendedor")
+
+function love.load()
+love.math.setRandomSeed(os.time())
+
+-- Temporizador
+t = 0
+-- Caracteristicas do jogardor 
+
+window_w = 1280
+window_h = 640
+-- Pontuçao do jogador
+
+dinheiro = 0
+
+player = {} 
+player.x = window_w/2
+player.y = window_h/2
+player.w = 10
+player.h = 20
+player.vel = 150
+player.d = 1
+player.hp = 100
+player.cd = 0.5
+
+monsters = {}
+vendedores = {}
+
+
+m_bullets = {}
+tiros = {}
+coins = {}
+
+mousex,mousey = 0, 0
+
+delay_player = player.cd
+
+delay_tiro = 0.25
+delay_tiro_aux = delay_tiro
+
+delay_carga = 0
+
+carga_total = 8
+carga_atual = 8
+
+--MENUS--
+menu = 0
+
+end
+
+function love.update(dt)
+    mousex,mousey = love.mouse.getPosition()
+    if menu == 1 then
+        --AREA CONTADORES GLOBAIS--
+        t = (t + dt*1 )
+        
+        delay_tiro = (delay_tiro - dt*1)
+        
+        delay_player = (delay_player - dt*1)       
+        
+        delay_carga = delay_carga -(dt * 1)
+
+
+        --AREA PLAYER--
+        movimento_player(dt, player.vel, window_w, window_h)
+        direcao_player(player)
+        
+
+        --AREA ARMAS--
+        
+        direcao_tiros(dt,tiros)
+        
+        if love.keyboard.isDown("r") and carga_atual < 8 and pode_recarregar then
+            delay_carga = 1.75  
+            pode_recarregar = false  
+        end 
+        if delay_carga < 0 then
+            pode_recarregar = true
+        end
+        if delay_carga > 0 and delay_carga < 0.1 then
+            carga_atual = carga_total 
+        end 
+
+        if delay_tiro < 0 and love.mouse.isDown(1) and carga_atual > 0 and delay_carga < 0 then
+            g_tiros(tiros, player.x, player.y, mousex, mousey)
+            carga_atual = carga_atual - 1
+            delay_tiro = delay_tiro_aux
+        end
+        
+        for i=#tiros, 1, -1 do 
+            local tiro = tiros[i]
+            for j=#monsters ,1 , -1 do 
+                local monster = monsters[j]
+                if AABB(tiro.x, monster.x, tiro.y, monster.y, tiro.w, monster.w, tiro.h, monster.h) then
+                    monster.hp = monster.hp - tiro.dano
+                    table.remove(tiros,i)
+                    
+                end
+            end
+        end
+        
+        bullet_death(tiros,window_w, window_h)
+        
+        --AREA VENDEDOR--(EM TESTE)
+        --[[
+        vendedor(t,vendedores)
+        
+        itens(vendedores)
+        
+        compras(vendedores,dinheiro,player)
+        ]]--
+
+        --AREA MONSTROS--
+        
+        g_monster(dt, 10, monsters,t, window_w, window_h)
+    
+        monster_move(dt, player.x, player.y, monsters)
+
+        monster_death(monsters, coins)
+        
+        for i=#monsters, 1, -1 do 
+            local monster = monsters[i]
+            
+            monster.delay = monster.delay - dt*1
+
+            if AABB(player.x, monster.x, player.y, monster.y, player.w, monster.w, player.h, monster.h) and delay_player < 0 then
+                player.hp = player.hp - monster.atq
+                delay_player = player.cd
+            end     
+            
+            if monster.tipo >= 2 and monster.delay <= 0 then 
+                local distancia = ((monster.x - player.x)^2 + (monster.y - player.y)^2)^(1/2)
+                if distancia <= 600 and distancia > 100 then
+                    gmonster_bullets(m_bullets,  monster.x, monster.y, player.x, player.y)
+                    monster.delay = 1.5 
+                end
+            end
+            
+        end
+
+        monster_bulletd(dt, m_bullets)
+
+        for i=#m_bullets, 1, -1 do
+            local monsterBullet = m_bullets[i]
+            
+            if AABB(player.x, monsterBullet.x, player.y, monsterBullet.y, player.w, monsterBullet.w, monsterBullet.h, monsterBullet.h) and delay_player < 0 then
+                player.hp = player.hp - monsterBullet.dano
+                delay_player = player.cd
+            end    
+            
+            if AABB(player.x, monsterBullet.x, player.y, monsterBullet.y, player.w, monsterBullet.w, monsterBullet.h, monsterBullet.h) then
+                table.remove(m_bullets, i) 
+            end
+
+            if monsterBullet.x > window_w or monsterBullet.x < -20 or monsterBullet.y > window_h or monsterBullet.y < -20 then
+                table.remove(m_bullets, i)
+            end
+        
+        end
+
+        mov_progressivo(dt,coins,player.x,player.y)
+
+        for i=#coins, 1, -1 do
+            local coin = coins[i]
+            if AABB(player.x, coin.x, player.y, coin.y, player.w, coin.w, player.h, coin.h) then
+                table.remove(coins, i)
+                dinheiro = dinheiro + 1
+            end
+        end
+
+        if player.hp <= 0 then
+            menu = 0
+            love.event.clear()
+        end
+
+    elseif menu == 0 then
+        
+        t = 0
+
+        dinheiro = 0
+
+        player = {} 
+        player.x = window_w/2
+        player.y = window_h/2
+        player.w = 10
+        player.h = 20
+        player.vel = 150
+        player.d = 1
+        player.hp = 100
+        player.cd = 0.5
+        
+        monsters = {}
+        vendedores = {}
+        
+        m_bullets = {}
+        tiros = {}
+        coins = {}
+
+        delay_carga = 0
+
+        carga_atual = 8
+        
+        if mousex > window_w/2 - 100 and mousex < window_w/2 + 100 and mousey > window_h/2 and mousey < window_h/2 + 50 and love.mouse.isDown(1) then
+            menu = 1
+        end
+    end
+    
+end
+
+function love.draw()
+    if menu == 1 then
+        love.graphics.setBackgroundColor(0,0,0)
+        
+        love.graphics.setColor(0.8 , 0.8, 0) 
+        draw_coins(coins)  
+        
+        love.graphics.setColor(0.8, 0.8, 0.8) 
+        bullet_draw(tiros)
+        
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.rectangle("fill", player.x, player.y, player.w, player.h)
+        draw_vendedor(vendedores,player.x, player.y)
+        draw_monster(monsters)
+        
+        love.graphics.setColor(0.86, 0.08, 0.23)
+        love.graphics.print("COINS : ".. dinheiro , 5, 20, 0)
+        love.graphics.print("TIMER : ".. math.floor(t), 5, 35, 0)
+        love.graphics.print("MUNICAO :".. carga_atual, window_w - 100, 0)
+        
+        if player.hp > 0 then
+            love.graphics.rectangle("fill", 5, 5, 150 *(player.hp/100),10 )
+        end
+        
+        if delay_carga <= 2.5 and delay_carga >= 0 then
+            love.graphics.rectangle("fill", player.x - 5, player.y - 15, 35*(delay_carga/2.5), 5)
+        end
+        
+        for i=#monsters, 1, -1 do
+            local monster = monsters[i]
+        end
+        
+        mbullet_draw(m_bullets)
+        
+        --[[ TESTES 
+        for i= #vendedores, 1, -1 do
+            local vendedor = vendedores[i]
+            love.graphics.print(#vendedor.itens,800,0)
+        end
+        ]]--
+    elseif menu == 0 then
+        love.graphics.setColor(1,1,1)
+        love.graphics.print("MINI HUNTER",window_w/2 - 200,0,r,5,15)
+        if mousex > window_w/2 - 100 and mousex < window_w/2 + 100 and mousey > window_h/2 and mousey < window_h/2 + 50 then
+            love.graphics.setColor(1,0,0)
+            love.graphics.rectangle("fill",window_w/2 - 100,window_h/2,200,50)
+        else
+            love.graphics.setColor(1,1,1)
+            love.graphics.rectangle("line",window_w/2 - 100,window_h/2,200,50)
+        end
+        love.graphics.setColor(1,1,1)
+        love.graphics.print("START",window_w/2 - 20, window_h/2 + 10,r,1,2)
+    end
+end
